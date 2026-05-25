850d08fc08ad2ba7e327c9a76d6de4678335ba18
TainoTech <rodriguez74@gmail.com>
2026-05-25 15:09:06 -0400

    Initial commit: add legacy Firebase web app and project README

---
+++ README.md
@@ -0,0 +1,26 @@
+# Praying 4 You
+
+Praying 4 You began as a simple Advanced JavaScript class project hosted on Firebase. The original web app allowed users to submit and view prayer requests.
+
+This repository preserves the original Firebase web version in `/legacy-web-app` and will be used to rebuild the app as a cross-platform React Native mobile application for iOS and Android.
+
+## Project Goals
+
+- Rebuild the app using React Native and Expo
+- Support user accounts with email-based authentication
+- Allow users to post prayer requests publicly or anonymously
+- Let users browse and pray for submitted requests
+- Add prayer counts and engagement features
+- Add reporting and moderation support for user-generated content
+- Prepare the app for iOS and Android deployment
+- Explore lightweight monetization through mobile ads
+
+## Repository Structure
+
+```text
+praying4you/
+  legacy-web-app/
+    Original Advanced JavaScript / Firebase web app
+
+  mobile-app/
+    Future React Native / Expo mobile app
---
+++ legacy-web-app/js/app-scratch.js
@@ -0,0 +1,101 @@
+/*----------------------------------------------------------------------------*/
+/*--- database stuff ...                                                   ---*/
+/*-----------------------------------"REPLACE_WITH_FIREBASE_API_KEY"*/
+  var config  =  {  apiKey         : "REPLACE_WITH_FIREBASE_API_KEY",
+                  databaseURL    : "REPLACE_WITH_FIREBASE_DATABASE_URL",
+                  storageBucket  : "REPLACE_WITH_FIREBASE_STORAGE_BUCKET"
+                 };
+  var data    =  {};
+
+  firebase.initializeApp( config );
+  var conn = firebase.database().ref('prayers/');
+  conn.on("child_added", function(snapshot) {
+     var prayer         = snapshot.val();
+     data[snapshot.key] = prayer;
+     var tr      = "<tr>";
+         tr     +=     "<td>" + prayer.date    + "</td>";
+         tr     +=     "<td>" + prayer.user    + "</td>";
+         tr     +=     "<td>" + prayer.prayer  + "</td>";
+         tr     +=     "<td>";
+         tr     +=        "<ul class='nav nav-pills'>";
+         tr     +=           "<li>";
+         tr     +=              "<a href='#'>";
+         tr     +=                 "<i onClick='vote(\"" + snapshot.key + "\");' class='fa fa-thumbs-up' aria-hidden='true'></i>";
+         tr     +=                 "<span id='"+snapshot.key+"' class='badge'>" + prayer.votes + "</span>";
+         tr     +=              "</a>";
+         tr     +=           "</li>";
+         tr     +=        "</ul>";
+         tr     +=     "</td>";
+         tr     += "</tr>";
+     $("#prayer-list").prepend( tr );
+  });
+
+  /*----------------------------------------------------------------------------*/
+  /*--- document ready ...                                                   ---*/
+  /*----------------------------------------------------------------------------*/
+  $(document).ready( function (){
+  		 bibleApiCall();
+  	 $("#submitprayer").on('click', save);
+  })
+
+  /*----------------------------------------------------------------------------*/
+  /*--- today ...                                                            ---*/
+  /*----------------------------------------------------------------------------*/
+  function today()
+  {
+     var date          = new Date();
+     var currentDate   = date.getDate();
+     var month         = date.getMonth() + 1;
+     var year          = date.getFullYear();
+     return month + "/" + currentDate + "/" + year;
+  }
+
+  /*----------------------------------------------------------------------------*/
+  /*--- save ...                                                             ---*/
+  /*----------------------------------------------------------------------------*/
+  function save( date, user, prayer, votes )
+  {
+     var user    = $('#inputUser').val();
+     var prayer  = $('#inputPrayer').val();
+     conn.push( { date    :  today(),
+                  user    :  user,
+                  prayer  :  prayer,
+                  votes   :  0
+              });
+  }
+
+  /*----------------------------------------------------------------------------*/
+  /*--- vote ...                                                             ---*/
+  /*----------------------------------------------------------------------------*/
+  function vote( k )
+  {
+     data[k].votes++;
+     conn.update(data);
+     $("#"+k).html( data[k].votes );
+  	 location.reload();
+  }
+  /*-------verse of the day API call -------*/
+  function bibleApiCall(){
+     var link = "http://labs.bible.org/api/?passage=votd&type=json";
+        $.ajax({
+           url         :  link,
+           method      :  'GET',
+           crossDomain :  true,
+           dataType    :  'jsonp',
+           success     :  function( r ) {
+              var   bookname = r[0].bookname;
+              var   chapter  = r[0].chapter;
+              var   verse    = r[0].verse;
+              var   text     = r[0].text;
+              var   content  = "<legend>Verse Of The Day</legend>";
+                    content +=    "<blockquote>";
+                    content +=    "<p id='bible'>" + text + "</p>";
+                    content +=    "<small>" + bookname + " " + chapter + ":" + verse + "<cite title='Source Title'>NIV</cite></small>";
+                    content += "</blockquote>";
+              $("#randomVerse").html( content );
+           },
+           error   : function( error ){
+              alert("opps something went wrong!")
+           }
+      });
+     }
